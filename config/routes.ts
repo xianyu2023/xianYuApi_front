5825ea91cfb655d11c94e665c3469d51aa955f66
xianyu <1802698990@qq.com>
2023-07-21 08:26:39 +0800

    Api开放平台-前端 首次提交

---
+++ config/routes.ts
@@ -0,0 +1,16 @@
+﻿export default [
+  {name: '登录', path: '/user', layout: false, routes: [{ path: '/user/login', component: './User/Login' }] },
+  {name: '欢迎页面', path: '/welcome', icon: 'smile', component: './Welcome' },
+  {name: '管理员页面',
+    path: '/admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      { path: '/admin', redirect: '/admin/sub-page' },
+      { path: '/admin/sub-page', component: './Admin' },
+    ],
+  },
+  {name: '表格页面', icon: 'table', path: '/list', component: './TableList' },
+  {path: '/', redirect: '/welcome' },
+  {path: '*', layout: false, component: './404' },
+];
